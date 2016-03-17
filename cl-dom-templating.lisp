3fa50737fe793fd21d5c7864ea2de45fda14839d
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2016-03-17 15:27:25 +0300

    Implement set-value and helpers. Add generic method markers.
    Implement the set-value method and helper functions
    
    Add (defgeneric ...) markers for html and set-value methods to
    prevent warnings
    
    Correct the DOCTYPE declaration

--- cl-dom-templating.lisp
+++ cl-dom-templating.lisp
@@ -14,12 +14,38 @@
   (let ((path (file-path template)))
     (setf (slot-value template 'dom) (cxml:parse (pathname path) (cxml-dom:make-dom-builder)))))
 
+(defgeneric html (template)
+  (:documentation "Outputs the DOM as a HTML string"))
+
+(defgeneric set-value (template &key x-path value)
+  (:documentation "Updates the value of the elements matching the given xpath expression"))
 
 (defmethod html ((template domtemplate))
   (with-output-to-string (s)
     (format s "~a~%~a~%"
-            "<!DOCTYPE htmlss>"
+            "<!DOCTYPE html>"
             (dom:map-document
              (cxml:make-string-sink :omit-xml-declaration-p t)
              (dom template)))
     s))
+
+(defmethod set-value ((template domtemplate) &key x-path value)
+  (loop for element in (get-dom-element-by-xpath template x-path)
+     do
+       (setf (dom:node-value element) value)))
+
+(defun get-dom-element-by-xpath (template x-path)
+  (let ((nodeset (xpath:evaluate x-path (dom template))))
+    (let ((iterator (xpath:make-node-set-iterator nodeset))
+          (elements ()))
+      (loop
+         for element = (xpath:node-set-iterator-current iterator)
+         then (advance-iterator-and-return-value iterator)
+         while element
+         do
+           (setf elements (append elements (list element))))
+      elements)))
+
+(defun advance-iterator-and-return-value (iterator)
+  (xpath:node-set-iterator-next iterator)
+  (xpath:node-set-iterator-current iterator))
